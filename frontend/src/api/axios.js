bd785289f9a955999e978abc18d2fe2497ec46f1
Elevate Tech <elevate.logy@gmail.com>
2025-05-09 14:39:42 -0600

    Add api real url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://to-do-nppa.onrender.com',
 });
 
 api.interceptors.request.use(
